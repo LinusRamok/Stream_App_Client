8f032151c671a8fc4d20a37c0250e53974b278a3
LinusRamok <linussunil.dsk@gmail.com>
2021-07-26 16:49:00 +0530

    Navigation with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,35 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/PageTwo">Link to Page Two</Link>
+    </div>
+  );
+};
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <Link to="/">Link to Page One</Link>
+    </div>
+  );
+};
 
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={PageOne} />
+            <Route path="/PageTwo" component={PageTwo} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
