1ddf59ce0bba65a6cc4ed34468df6c6da5dd1230
LinusRamok <linussunil.dsk@gmail.com>
2021-07-30 15:21:16 +0530

    created actions and reducer for all stream components

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,23 @@
+import {
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+  FETCH_STREAM,
+} from "../actions/type";
+import _ from "lodash";
+
+export const streamReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+    case CREATE_STREAM:
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(state, "id") };
+    default:
+      return state;
+  }
+};
